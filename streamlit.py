a3c748c1d751f92a78cf41fcf68bd427788275aa
Luizfsbessa <luizfsbessa@gmail.com>
2024-11-27 09:40:02 -0300

    Update streamlit.py

--- streamlit.py
+++ streamlit.py
@@ -111,113 +111,61 @@ else:
     if end_date:
         filtered_df = filtered_df[filtered_df['Data de abertura'] <= pd.to_datetime(end_date)]
 
-    # Calcular o total de horas por tipo
+    # Gráficos de número de atendimentos por mês, separados por Tipo (Requisição e Incidente)
     incidentes_df = filtered_df[filtered_df['Tipo'] == 'Incidente']
     requisicoes_df = filtered_df[filtered_df['Tipo'] == 'Requisição']
 
-    # Exibir os tempos em atendimento com informações detalhadas de prioridade
-    if not incidentes_df.empty:
-        # Cálculo de tempos médios por prioridade em Incidentes
-        tempos_incidentes = incidentes_df.groupby('Prioridade')['Horas Decimais'].agg(['mean', 'max']).reset_index()
-        tempos_incidentes['Média'] = tempos_incidentes['mean'].apply(format_hours_to_hms)
-        tempos_incidentes['Máximo'] = tempos_incidentes['max'].apply(format_hours_to_hms)
-
-        # Gerar o texto com os detalhes
-        incidentes_detalhes = "".join([  
-            f"<li><b>{row['Prioridade']}:</b> Média: {row['Média']} | Máximo: {row['Máximo']}</li>"
-            for _, row in tempos_incidentes.iterrows()
-        ])
-
-        st.markdown(
-            f"""
-            <div style='background-color: #C1D8E3; padding: 15px; border-radius: 5px; margin-bottom: 10px;'>
-                <b>Tempo total em Incidentes:</b> {format_hours_to_hms(incidentes_df['Horas Decimais'].sum())}
-                <ul>
-                    {incidentes_detalhes}
-                </ul>
-            </div>
-            """,
-            unsafe_allow_html=True
-        )
-
-    if not requisicoes_df.empty:
-        # Cálculo de tempos médios por prioridade em Requisições
-        tempos_requisicoes = requisicoes_df.groupby('Prioridade')['Horas Decimais'].agg(['mean', 'max']).reset_index()
-        tempos_requisicoes['Média'] = tempos_requisicoes['mean'].apply(format_hours_to_hms)
-        tempos_requisicoes['Máximo'] = tempos_requisicoes['max'].apply(format_hours_to_hms)
-
-        # Gerar o texto com os detalhes
-        requisicoes_detalhes = "".join([  
-            f"<li><b>{row['Prioridade']}:</b> Média: {row['Média']} | Máximo: {row['Máximo']}</li>"
-            for _, row in tempos_requisicoes.iterrows()
-        ])
-
-        st.markdown(
-            f"""
-            <div style='background-color: #C1D8E3; padding: 15px; border-radius: 5px; margin-bottom: 10px;'>
-                <b>Tempo total em Requisições:</b> {format_hours_to_hms(requisicoes_df['Horas Decimais'].sum())}
-                <ul>
-                    {requisicoes_detalhes}
-                </ul>
-            </div>
-            """,
-            unsafe_allow_html=True
-        )
-
-    # Gráficos de número de atendimentos por mês, separados por Tipo (Requisição e Incidente)
+    # Gráfico de barras de Incidentes
     incidentes_por_mes = incidentes_df.groupby('Mês/Ano').size().reset_index(name='Número de Atendimentos')
+    fig_incidentes = px.bar(
+        incidentes_por_mes,
+        x='Mês/Ano',
+        y='Número de Atendimentos',
+        text='Número de Atendimentos',
+        title="Número de Atendimentos por Mês - Incidentes",
+        color_discrete_sequence=['#1EA4B6']  # Cor para Incidentes
+    )
+    fig_incidentes.update_traces(texttemplate='<b>%{text}</b>', textposition='outside')
+    fig_incidentes.update_layout(
+        xaxis_title=None,
+        yaxis_title=None,
+        showlegend=False,
+        plot_bgcolor='rgba(0,0,0,0)',  # Fundo transparente
+        paper_bgcolor='rgba(0,0,0,0)',  # Fundo transparente
+        font=dict(color="black"),  # Cor do texto do gráfico
+    )
+    fig_incidentes.update_xaxes(showgrid=False)
+    fig_incidentes.update_yaxes(
+        showgrid=False,      # Opcional: remove a grade do eixo Y
+        zeroline=False,      # Opcional: remove a linha zero do eixo Y
+    )
+    st.plotly_chart(fig_incidentes)
+
+    # Gráfico de barras de Requisições
     requisicoes_por_mes = requisicoes_df.groupby('Mês/Ano').size().reset_index(name='Número de Atendimentos')
-
-    # Verificar se os DataFrames não estão vazios e exibir os gráficos de barras
-    if not incidentes_por_mes.empty:
-        fig_incidentes = px.bar(
-            incidentes_por_mes,
-            x='Mês/Ano',
-            y='Número de Atendimentos',
-            text='Número de Atendimentos',
-            title="Número de Atendimentos por Mês - Incidentes",
-            color_discrete_sequence=['#1EA4B6']  # Cor para Incidentes
-        )
-        fig_incidentes.update_traces(texttemplate='<b>%{text}</b>', textposition='outside')
-        fig_incidentes.update_layout(
-            xaxis_title=None,
-            yaxis_title=None,
-            showlegend=False,
-            plot_bgcolor='rgba(0,0,0,0)',  # Fundo transparente
-            paper_bgcolor='rgba(0,0,0,0)',  # Fundo transparente
-            font=dict(color="black"),  # Cor do texto do gráfico
-        )
-        fig_incidentes.update_xaxes(showgrid=False)
-        fig_incidentes.update_yaxes(
-            showgrid=False,      # Opcional: remove a grade do eixo Y
-            zeroline=False,      # Opcional: remove a linha zero do eixo Y
-        )
-        st.plotly_chart(fig_incidentes)
-
-    if not requisicoes_por_mes.empty:
-        fig_requisicoes = px.bar(
-            requisicoes_por_mes,
-            x='Mês/Ano',
-            y='Número de Atendimentos',
-            text='Número de Atendimentos',
-            title="Número de Atendimentos por Mês - Requisições",
-            color_discrete_sequence=['#00A86B']  # Cor para Requisições
-        )
-        fig_requisicoes.update_traces(texttemplate='<b>%{text}</b>', textposition='outside')
-        fig_requisicoes.update_layout(
-            xaxis_title=None,
-            yaxis_title=None,
-            showlegend=False,
-            plot_bgcolor='rgba(0,0,0,0)',  # Fundo transparente
-            paper_bgcolor='rgba(0,0,0,0)',  # Fundo transparente
-            font=dict(color="black"),  # Cor do texto do gráfico
-        )
-        fig_requisicoes.update_xaxes(showgrid=False)
-        fig_requisicoes.update_yaxes(
-            showgrid=False,      # Opcional: remove a grade do eixo Y
-            zeroline=False,      # Opcional: remove a linha zero do eixo Y
-        )
-        st.plotly_chart(fig_requisicoes)
+    fig_requisicoes = px.bar(
+        requisicoes_por_mes,
+        x='Mês/Ano',
+        y='Número de Atendimentos',
+        text='Número de Atendimentos',
+        title="Número de Atendimentos por Mês - Requisições",
+        color_discrete_sequence=['#00A86B']  # Cor para Requisições
+    )
+    fig_requisicoes.update_traces(texttemplate='<b>%{text}</b>', textposition='outside')
+    fig_requisicoes.update_layout(
+        xaxis_title=None,
+        yaxis_title=None,
+        showlegend=False,
+        plot_bgcolor='rgba(0,0,0,0)',  # Fundo transparente
+        paper_bgcolor='rgba(0,0,0,0)',  # Fundo transparente
+        font=dict(color="black"),  # Cor do texto do gráfico
+    )
+    fig_requisicoes.update_xaxes(showgrid=False)
+    fig_requisicoes.update_yaxes(
+        showgrid=False,      # Opcional: remove a grade do eixo Y
+        zeroline=False,      # Opcional: remove a linha zero do eixo Y
+    )
+    st.plotly_chart(fig_requisicoes)
 
     # Gráfico de pizza de prioridades
     if not df.empty:
@@ -235,4 +183,3 @@ else:
             }
         )
         st.plotly_chart(fig_pizza)
-
